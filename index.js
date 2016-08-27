3a85a13059d679c594b70330eeddaa2af6086980
Denis Storti Da Silva <denstorti@users.noreply.github.com>
2016-08-26 22:59:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,36 @@
 var AWS = require('aws-sdk');
-
 AWS.config.update({region:'us-west-2'});
-//var kinesis = new AWS.Kinesis();
 var firehose = new AWS.Firehose();
+
 var Twitter = require('twitter');
  
 var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+  consumer_key: 'YOUR_CONSUMER_KEY',
+  consumer_secret: 'YOUR_CONSUMER_SECRET',
+  access_token_key: 'YOUR_ACCESS_TOKEN',
+  access_token_secret: 'YOUR_TOKEN_SECRET'
 });
-
 
 var stream = client.stream('statuses/filter', {track: '#ifood'});
 
 stream.on('data', function(event) {
 
-var params = {
-  DeliveryStreamName: 'teste-twitter', /* required */
-  Record: { /* required */
-    Data: new Buffer(event.text) || 'STRING_VALUE' /* required */
-  }
-};
+  var params = {
+    DeliveryStreamName: 'teste-twitter', /* required */
+    Record: { /* required */
+      Data: new Buffer(event.text) || 'STRING_VALUE' /* required */
+    }
+  };
 
    firehose.putRecord(params, function(err, data) {
      if (err) console.log(err, err.stack); // an error occurred
      else     console.log(data);           // successful response
    });
 
-
   console.log(event && event.text);
-
-
 });
  
 stream.on('error', function(error) {
   throw error;
 });
  
-// You can also get the stream in a callback if you prefer. 
-// client.stream('statuses/filter', {track: 'javascript'}, function(stream) {
-//   stream.on('data', function(event) {
-//       console.log(event && event.text);
-//         });
-//          
-//            stream.on('error', function(error) {
-//                throw error;
-//                  });
-//                  });
